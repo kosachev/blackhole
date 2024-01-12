db0c265a36e2d2d11f432d2f64b4aab7cf3a4767
shevernitskiy <mahahuha@ya.ru>
2024-01-12 13:50:40 +0300

    add cron service draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
 import { CdekModule } from "./cdek/cdek.module";
 import { LoggerMiddleware } from "./utils/logger.middleware";
-import { AmoModule } from './amo/amo.module';
+import { AmoModule } from "./amo/amo.module";
+import { CronService } from "./cron/cron.service";
 
 @Module({
   imports: [
     CdekModule,
+    AmoModule,
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: [".env.dev", ".env.prod"],
       isGlobal: true,
     }),
-    AmoModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [CronService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
