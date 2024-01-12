168e3681ff53d8816972e828f4857027eb04b20e
shevernitskiy <mahahuha@ya.ru>
2024-01-12 10:37:31 +0300

    add basic draft e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,92 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "../src/app.module";
+import { INestApplication } from "@nestjs/common";
+import * as pactum from "pactum";
 
-describe('AppController (e2e)', () => {
+describe("App e2e", () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const module_ref = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module_ref.createNestApplication();
+
     await app.init();
+    await app.listen(process.env.PORT ?? 6969);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it.todo("should pass");
+
+  describe("CDEK", () => {
+    describe("webhook", () => {
+      const test_order = {
+        type: "ORDER_STATUS",
+        date_time: "2020-08-10T21:32:14+0700",
+        uuid: "72753031-2801-4186-a091-0be58cedfee7",
+        attributes: {
+          is_return: false,
+          cdek_number: "1106321645",
+          code: "RECEIVED_AT_SHIPMENT_WAREHOUSE ",
+          status_code: "3",
+          status_date_time: "2020-08-10T21:32:12+0700",
+          city_name: "Новосибирск",
+          city_code: "270",
+        },
+      };
+      it("should return 201", () => {
+        return pactum
+          .spec()
+          .post("http://localhost:6969/cdek/webhook")
+          .withBody(test_order)
+          .expectStatus(201);
+      });
+    });
+  });
+
+  describe("AMO", () => {
+    describe("webhook", () => {
+      it("should return 201", () => {
+        const test_task = {
+          task: {
+            update: [
+              {
+                id: 11122233,
+                element_id: 33322211,
+                element_type: 2,
+                task_type: 1,
+                date_create: "2017-07-20 15:00:00",
+                text: "Follow-up",
+                status: 1,
+                account_id: 77711122,
+                created_user_id: 123123,
+                last_modified: "2017-07-21 19:00:00",
+                responsible_user_id: 123123,
+                complete_till: "2017-07-22 23:59:00",
+                action_close: 1,
+                result: {
+                  id: 155155155,
+                  text: "Success",
+                },
+              },
+            ],
+          },
+          account: {
+            subdomain: "test",
+          },
+        };
+
+        return pactum
+          .spec()
+          .post("http://localhost:6969/amo/webhook")
+          .withBody(test_task)
+          .expectStatus(201);
+      });
+    });
   });
 });
