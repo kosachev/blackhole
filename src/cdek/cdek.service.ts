b2f2c5c0526ea354a38d4753144244141fe581dc
shevernitskiy <mahahuha@ya.ru>
2024-01-22 17:34:54 +0300

    remove redundant code

--- src/cdek/cdek.service.ts
+++ src/cdek/cdek.service.ts
@@ -1,11 +1,4 @@
 import { Cdek } from "cdek";
-import {
-  UpdateDownloadPhoto,
-  UpdateOrderStatus,
-  UpdatePrealertClosed,
-  UpdatePrintForm,
-} from "cdek/src/types/api/webhook";
-
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
@@ -23,34 +16,9 @@ export class CdekService {
       ),
       on_error: (error) => this.logger.error(error),
     });
-
-    this.instance.on("ORDER_STATUS", (ctx: UpdateOrderStatus) => this.order_status(ctx));
-    this.instance.on("PRINT_FORM", (ctx: UpdatePrintForm) => this.print_form(ctx));
-    this.instance.on("DOWNLOAD_PHOTO", (ctx: UpdateDownloadPhoto) => this.download_photo(ctx));
-    this.instance.on("PREALERT_CLOSED", (ctx: UpdatePrealertClosed) => this.prealert_closed(ctx));
   }
 
   get client(): Cdek {
     return this.instance;
-  }
-
-  // TODO: implement logic
-  private order_status(ctx: UpdateOrderStatus): void {
-    this.logger.log(`order ${ctx.attributes.cdek_number} => status ${ctx.attributes.code}`);
-  }
-
-  // TODO: implement logic
-  private print_form(ctx: UpdatePrintForm): void {
-    this.logger.log(`print_form ${ctx}`);
-  }
-
-  // TODO: implement logic
-  private download_photo(ctx: UpdateDownloadPhoto): void {
-    this.logger.log(`download_photo ${ctx}`);
-  }
-
-  // TODO: implement logic
-  private prealert_closed(ctx: UpdatePrealertClosed): void {
-    this.logger.log(`prealert_closed ${ctx}`);
   }
 }
