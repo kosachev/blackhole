decf0db524c5f69a21ce1332a2d1af6aafee7af6
shevernitskiy <mahahuha@ya.ru>
2024-02-02 14:49:34 +0300

    you should bypass mfk

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -9,19 +9,19 @@ export class GoogleSheetsService {
   private doc: GoogleSpreadsheet;
 
   constructor(private readonly config: ConfigService) {
-    if (process.env.NODE_ENV !== "testing") {
-      this.doc = new GoogleSpreadsheet(
-        this.config.get<string>("GOOGLE_SPREADSHEET_ID"),
-        new JWT({
-          email: this.config.get<string>("GOOGLE_SHEETS_EMAIL"),
-          key: this.config.get<string>("GOOGLE_SHEETS_KEY"),
-          scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-        }),
-      );
-      // hope service will not be called before loadInfo ends, or do it in every method
+    if (this.config.get<string>("NODE_ENV") === "testing") return;
 
-      this.doc.loadInfo();
-    }
+    this.doc = new GoogleSpreadsheet(
+      this.config.get<string>("GOOGLE_SPREADSHEET_ID"),
+      new JWT({
+        email: this.config.get<string>("GOOGLE_SHEETS_EMAIL"),
+        key: this.config.get<string>("GOOGLE_SHEETS_KEY"),
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+      }),
+    );
+    // hope service will not be called before loadInfo ends, or do it in every method
+
+    this.doc.loadInfo();
   }
 
   async addSell(data: any): Promise<void> {
