41e6fa9a50ff43a4a98b77dac5f86fe53f86e79a
shevernitskiy <mahahuha@ya.ru>
2024-02-06 09:36:25 +0300

    fix logs path, remove spam from cronjob

--- src/cron/jobs/test.cron.ts
+++ src/cron/jobs/test.cron.ts
@@ -4,6 +4,6 @@ import { CronService } from "../cron.service";
 export class TestJob extends CronService {
   @Interval(10000)
   testjob(): void {
-    this.logger.debug("from test job");
+    // this.logger.debug("from test job");
   }
 }
