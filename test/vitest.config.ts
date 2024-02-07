d12128043c5309578d7a2d4226ca7483dccb6040
shevernitskiy <mahahuha@ya.ru>
2024-02-07 12:46:59 +0300

    change vitest config path

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     environment: "node",
     coverage: {
       provider: "v8",
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "json-summary", "json"],
       reportOnFailure: true,
     },
     reporters: "verbose",
