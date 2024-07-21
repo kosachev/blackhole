3231c077a711997f369fefd4863f8b8777ef1b07
shevernitskiy <mahahuha@ya.ru>
2024-07-21 08:00:59 +0300

    implement `web/cdek_pickup` endpoint

--- src/web/web.controller.ts
+++ src/web/web.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { RequestPartialReturn, PartialReturnService } from "./partial-return.service";
+import { CdekPickupService, RequestCdekPickup } from "./cdek-pickup.service";
 
 @Controller("web")
 export class WebController {
-  constructor(private readonly partial_return: PartialReturnService) {}
+  constructor(
+    private readonly partial_return: PartialReturnService,
+    private readonly cdek_pickup: CdekPickupService,
+  ) {}
 
   @Post("partial_return")
-  async request(@Body() data: RequestPartialReturn) {
+  async partialReturn(@Body() data: RequestPartialReturn) {
     return this.partial_return.handler(data);
+  }
+
+  @Post("cdek_pickup")
+  async cdekPickup(@Body() data: RequestCdekPickup) {
+    return this.cdek_pickup.handler(data);
   }
 }
--- src/web/web.module.ts
+++ src/web/web.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { WebController } from "./web.controller";
 import { PartialReturnService } from "./partial-return.service";
+import { CdekPickupService } from "./cdek-pickup.service";
 
 @Module({
-  providers: [PartialReturnService],
+  providers: [PartialReturnService, CdekPickupService],
   controllers: [WebController],
 })
 export class WebModule {}
