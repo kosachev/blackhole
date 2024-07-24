10d6f036ee23a6202773e636e5c200ebdb0365e2
shevernitskiy <mahahuha@ya.ru>
2024-07-24 07:18:37 +0300

    resolve confilct

--- src/log-viewer/log-viewer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { LogViewerController } from "./log-viewer.controller";
-import { LogViewerService } from "./log-viewer.service";
-
-@Module({
-  controllers: [LogViewerController],
-  providers: [LogViewerService],
-})
-export class LogViewerModule {}
