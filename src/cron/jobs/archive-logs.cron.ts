7bee6fe0c92c1baedd464f376696e082301fd9e3
shevernitskiy <mahahuha@ya.ru>
2024-09-02 20:31:57 +0300

    refactor monthly logs archiver

--- src/cron/jobs/archive-logs.cron.ts
+++ src/cron/jobs/archive-logs.cron.ts
@@ -7,19 +7,24 @@ import { Cron } from "@nestjs/schedule";
 import { CronService } from "../cron.service";
 
 export class ArchiveLogsJob extends CronService {
-  @Cron("0 0 1 1 * *")
+  @Cron("0 0 9 1 * *")
   async archiveJob(): Promise<void> {
     const date = new Date();
 
-    const today = date.toISOString().split("T")[0] + ".log";
     date.setMonth(date.getMonth() - 1);
     const prev_month = `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? "0" : ""}${date.getMonth() + 1}`;
+
     const archive = fs.createWriteStream(
       path.join(this.config.get<string>("LOG_ARCHIVE_PATH"), `${prev_month}.tar.gz`),
     );
     const gzip = zlib.createGzip();
     gzip.pipe(archive);
-    tar.pack("./logs", { filter: (name) => path.basename(name) === today }).pipe(gzip);
+    tar
+      .pack("./logs", {
+        filter: (name) =>
+          !path.basename(name).startsWith(prev_month) || !path.basename(name).endsWith(".log"),
+      })
+      .pipe(gzip);
 
     await new Promise((resolve, reject) => {
       archive.on("finish", resolve);
@@ -27,10 +32,10 @@ export class ArchiveLogsJob extends CronService {
     });
 
     for (const file of fs.readdirSync("./logs")) {
-      if (file === today) continue;
+      if (!file.startsWith(prev_month) || !file.endsWith(".log")) continue;
       fs.unlinkSync(`./logs/${file}`);
     }
 
-    this.logger.log(`Logs archived to ${prev_month}.tar.gz`);
+    this.logger.log(`Logs archived to ${archive.path}`);
   }
 }
