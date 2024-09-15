cb1323bb959fd76ee7674298cb712cb90da45d32
shevernitskiy <mahahuha@ya.ru>
2024-09-15 08:43:45 +0300

    amo: add `lead-create` service

--- src/amo/amo.controller.ts
+++ src/amo/amo.controller.ts
@@ -3,31 +3,41 @@ import { AutoOkResponse } from "../utils/auto-ok-response.interceptor";
 import { LeadStatusWebhook } from "./webhooks/lead-status.webhook";
 import { LeadAddWebhook } from "./webhooks/lead-add.webhook";
 import { LeadChangeWebhook } from "./webhooks/lead-change.webhook";
+import { LeadCreateService, type Order } from "./lead-create.service";
 
-@UseInterceptors(AutoOkResponse)
 @Controller("amo")
 export class AmoController {
   constructor(
     private readonly lead_status: LeadStatusWebhook,
     private readonly lead_add: LeadAddWebhook,
     private readonly lead_change: LeadChangeWebhook,
+    private readonly lead_create: LeadCreateService,
   ) {}
 
+  @UseInterceptors(AutoOkResponse)
   @Post("lead_status")
   async leadStatus(@Body() data: any): Promise<string> {
     await this.lead_status.handle(data);
     return "OK";
   }
 
+  @UseInterceptors(AutoOkResponse)
   @Post("lead_add")
   async leadAdd(@Body() data: any): Promise<string> {
     await this.lead_add.handle(data);
     return "OK";
   }
 
+  @UseInterceptors(AutoOkResponse)
   @Post("lead_change")
   async leadChange(@Body() data: any): Promise<string> {
     await this.lead_change.handle(data);
+    return "OK";
+  }
+
+  @Post("lead_create")
+  async leadCreate(@Body() data: Order): Promise<string> {
+    await this.lead_create.handle(data);
     return "OK";
   }
 }
