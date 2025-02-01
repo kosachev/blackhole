4d714abadf0249402e80e04647d1ddabb135d2a3
shevernitskiy <mahahuha@ya.ru>
2025-02-01 20:00:42 +0300

    update tests

--- test/cdek/order-status.webhook.spec.ts
+++ test/cdek/order-status.webhook.spec.ts
@@ -2,7 +2,7 @@ import { mockAmoService } from "test/mocks/amo.mock";
 import { order_status_factory } from "test/mocks/cdek.mock";
 import { mockGoogleSheetsService } from "test/mocks/google-sheets.mock";
 import { mockMailService } from "test/mocks/mail.mock";
-import { describe, test, expect, beforeAll, afterAll } from "vitest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 
 import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
@@ -38,11 +38,15 @@ describe("CDEK OrderStatusWebhook", () => {
   test("1/20", () => {
     expect(service.parse(order_status_factory(1, 20))).toStrictEqual({
       custom_fields: [
-        [1997433, `1/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`],
+        [
+          1997433,
+          `1/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+        ],
         [1430854, "1197739374"],
         [1997427, "https://lk.cdek.ru/print/print-order?numberOrd=1197739374"],
       ],
-      note: `✎ СДЭК: получен трек-код 1197739374, накладная https://lk.cdek.ru/print/print-order?numberOrd=1197739374 (1)`,
+      note:
+        `✎ СДЭК: получен трек-код 1197739374, накладная https://lk.cdek.ru/print/print-order?numberOrd=1197739374 (1)`,
       tag: [],
     });
   });
@@ -50,7 +54,10 @@ describe("CDEK OrderStatusWebhook", () => {
   test("3/22", () => {
     expect(service.parse(order_status_factory(3, 20))).toStrictEqual({
       tag: [],
-      custom_fields: [[1997433, `3/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`]],
+      custom_fields: [[
+        1997433,
+        `3/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+      ]],
       note: "ℹ СДЭК: посылка принята на склад отправителя (3)",
       status: AMO.STATUS.SENT,
     });
@@ -59,8 +66,14 @@ describe("CDEK OrderStatusWebhook", () => {
   test("5/15", () => {
     expect(service.parse(order_status_factory(5, 15))).toStrictEqual({
       tag: [AMO.TAG.RETURN],
-      custom_fields: [[1997433, `5/15, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`]],
-      note: "ℹ СДЭК: посылка не вручена адресату (5) по причине отказа из-за того, что не устроили сроки (15)",
+      custom_fields: [[
+        1997433,
+        `5/15, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+      ]],
+      note:
+        "ℹ СДЭК: посылка не вручена адресату (5) по причине отказа из-за того, что не устроили сроки (15)\n⇌ СДЕК ВОЗВРАТ: Сделка переведена в возвраты",
+      pipeline: AMO.PIPELINE.RETURN,
+      status: AMO.STATUS.RETURN,
       // status: AMO.STATUS.CLOSED,
       // loss_reason: AMO.LOSS_REASON.RETURN,
     });
@@ -69,25 +82,37 @@ describe("CDEK OrderStatusWebhook", () => {
   test("5/20", () => {
     expect(service.parse(order_status_factory(5, 20))).toStrictEqual({
       tag: [AMO.TAG.RETURN],
-      custom_fields: [[1997433, `5/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`]],
-      note: "ℹ СДЭК: посылка не вручена адресату (5) по причине частичной доставки (20)",
+      custom_fields: [[
+        1997433,
+        `5/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+      ]],
+      note:
+        "ℹ СДЭК: посылка не вручена адресату (5) по причине частичной доставки (20)\n⇌ СДЕК ВОЗВРАТ: Сделка переведена в возвраты",
+      pipeline: AMO.PIPELINE.RETURN,
+      status: AMO.STATUS.RETURN,
     });
   });
 
   test("4", () => {
     expect(service.parse(order_status_factory(4))).toStrictEqual({
       tag: [],
-      custom_fields: [[1997433, `4, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`]],
+      custom_fields: [[
+        1997433,
+        `4, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+      ]],
       note: "✔ СДЭК: посылка успешно вручена адресату (4)",
       status: AMO.STATUS.SUCCESS,
     });
   });
 
-  test("4/20", () => {
-    expect(service.parse(order_status_factory(4, 20))).toStrictEqual({
-      tag: [AMO.TAG.PARTIAL_RETURN],
-      custom_fields: [[1997433, `4/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`]],
-      note: "✔ СДЭК: частичный выкуп товаров адресатом (4/20)",
-    });
-  });
+  // test("4/20", () => {
+  //   expect(service.parse(order_status_factory(4, 20))).toStrictEqual({
+  //     tag: [AMO.TAG.PARTIAL_RETURN],
+  //     custom_fields: [[
+  //       1997433,
+  //       `4/20, Набережные Челны, ${new Date().toLocaleString("ru-RU")}`,
+  //     ]],
+  //     note: "✔ СДЭК: частичный выкуп товаров адресатом (4/20)",
+  //   });
+  // });
 });
